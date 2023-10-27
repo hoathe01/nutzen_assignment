fd5d22d56b12ce6bdcfaf3f780736f87b4109474
Maliketh <hoathe8945@gmail.com>
2023-10-27 16:49:28 +0700

    Huynh The Hoa

---
+++ Server/src/main/java/com/huynhthehoa/nutzen/NutzenApplication.java
@@ -0,0 +1,13 @@
+package com.huynhthehoa.nutzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutzenApplication.class, args);
+	}
+
+}
